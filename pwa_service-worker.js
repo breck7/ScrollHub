5935e5766acd58276107c3aa70666df23303e3d1
bardicreels <180100244+bardicreels@users.noreply.github.com>
2024-11-06 12:17:12 +0000

    Create pwa_service-worker.js

---
+++ pwa_service-worker.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('my-cache').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/script.js',
+        '/icon-192x192.png',
+        '/icon-512x512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
